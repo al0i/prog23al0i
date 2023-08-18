aba735a8a01c78f22b83f50edb0a1107a896f667
Matheus William Joenck <117489072+al0i@users.noreply.github.com>
2023-08-18 10:28:01 -0300

    Add files via upload

--- trash/game.py
+++ trash/game.py
@@ -7,10 +7,8 @@ window = pygame.display.set_mode((width, height))
 clock = pygame.time.Clock()
 pygame.display.set_caption("al0i's game")
 
-class Player:
-    def __init__(self):
-        self.x = 
-    pygame.draw.circle(window, (255,0,0), [640,360], 90, 0)
+x,y = 640,360
+vel = 1
 
 while True:
     for event in pygame.event.get():
@@ -18,8 +16,17 @@ while True:
             pygame.quit()
             raise SystemExit
     
-    window.fill('white')
+    move = pygame.key.get_pressed()
+    if move[pygame.K_w] or move[pygame.K_UP]:
+        y -= vel
+    if move[pygame.K_s] or move[pygame.K_DOWN]:
+        y += vel
+    if move[pygame.K_d] or move[pygame.K_RIGHT]:
+        x += vel
+    if move[pygame.K_a] or move[pygame.K_LEFT]:
+        x -= vel
 
-    player(window)
+    window.fill('white')
+    pygame.draw.circle(window, (255,0,0), [x,y], 90, 0)
     pygame.display.flip()
     clock.tick(60)
