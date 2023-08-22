8e410bdb3075575a9d3c7a8f3a36aeacd0dad2aa
Matheus William Joenck <117489072+al0i@users.noreply.github.com>
2023-08-22 20:30:11 -0300

    Add files via upload

--- trash/game.py
+++ trash/game.py
@@ -1,4 +1,5 @@
 import pygame
+import os
 
 pygame.init()
 
@@ -7,26 +8,74 @@ window = pygame.display.set_mode((width, height))
 clock = pygame.time.Clock()
 pygame.display.set_caption("al0i's game")
 
-x,y = 640,360
-vel = 1
+class Player(pygame.sprite.Sprite):
+    def __init__(self, x, height):
+        pygame.sprite.Sprite.__init__(self)
+        self.sprites = []
+        self.sprites.append(pygame.image.load('mamaco.jpg'))
+        self.numb = 0
+        self.image = self.sprites[self.numb]
+        self.image = pygame.transform.scale(self.image, (550/3,641/3))
+        self.speed = 15
+        self.radius = 10
+        self.x = x
+        self.y = height#-self.radius
+        self.rect = self.image.get_rect()
+
+    def move(self):
+        move = pygame.key.get_pressed()
+        if move[pygame.K_LEFT] or move[pygame.K_a]:
+            if player.x < 0+player.radius:
+                pass
+            else:
+                player.x -= player.speed
+
+        if move[pygame.K_RIGHT] or move[pygame.K_d]:
+            if player.x > width+player.radius:
+                pass
+            else:
+                player.x += player.speed
+
+        if move[pygame.K_UP] or move[pygame.K_w]:
+            if player.y > width+player.radius:
+                pass
+            else:
+                player.y -= player.speed
+
+        if move[pygame.K_DOWN] or move[pygame.K_s]:
+            if player.y > width+player.radius:
+                pass
+            else:
+                player.y += player.speed
+
+        if move[pygame.K_SPACE]:
+            player.radius += 1
+
+    def update(self):
+        self.move()
+
+x = 640
+sprites = pygame.sprite.Group()
+player = Player(x, height)
+sprites.add(player)
 
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             raise SystemExit
+        
     
-    move = pygame.key.get_pressed()
-    if move[pygame.K_w] or move[pygame.K_UP]:
-        y -= vel
-    if move[pygame.K_s] or move[pygame.K_DOWN]:
-        y += vel
-    if move[pygame.K_d] or move[pygame.K_RIGHT]:
-        x += vel
-    if move[pygame.K_a] or move[pygame.K_LEFT]:
-        x -= vel
+    
+
+    print(player.x, player.y)
 
     window.fill('white')
-    pygame.draw.circle(window, (255,0,0), [x,y], 90, 0)
+    sprites.update()
+    sprites.draw(window)
+
+    pygame.draw.circle(window, (255,0,0), [player.x,player.y], player.radius, 0)
+    sprites.draw(window)
+    sprites.update()
     pygame.display.flip()
     clock.tick(60)
